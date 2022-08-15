07397403ca846f218437112218ecf1ac8229f53b
shn <topdog.shn.01@gmail.com>
2022-08-15 22:36:18 +0900

    ✨ Blog一覧：仮実装

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,96 @@
+// import Link from "next/link";
+import React from "react";
+
+export const Blog = () => {
+	return (
+		<section className="mb-24 ml-20 mt-10">
+			<h2 className="mb-6 border-b border-gray-200 pb-5 text-left text-2xl font-bold">
+				Blog
+			</h2>
+			<ul>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+				<li className="mb-6">
+					<h3 className="mb-2 text-[22px]">This is a header</h3>
+					<p className="mb-2">
+						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+						sint. Velit officia consequat duis enim velit mollit.
+					</p>
+					<p className="text-bold text-gray-400">2022.07.11</p>
+				</li>
+			</ul>
+		</section>
+	);
+};
+
+export default Blog;
