25881f5f2b591247688fff62f198210674ad747a
shn <topdog.shn.01@gmail.com>
2022-08-15 22:34:07 +0900

    🎨 構造改修

--- src/layout/AppLayout/index.tsx
+++ src/layout/AppLayout/index.tsx
@@ -1,24 +1,12 @@
 import React from "react";
 import { Footer } from "src/layout/AppLayout/Footer";
 import { Header } from "src/layout/AppLayout/Header";
-import { TitleSection } from "src/component/TitleSection";
-import { BlogSection } from "src/component/BlogSection";
-import { PortfolioSection } from "src/component/PortfolioSection";
-import { GithubSection } from "src/component/GithubSection";
-import { TwitterSection } from "src/component/TwitterSection";
 
 export const AppLayout = (props: any) => {
 	return (
 		<div className="mx-auto flex min-h-screen flex-col items-center">
 			<Header />
-			<TitleSection />
-			<BlogSection />
-			<PortfolioSection />
-			<div className="mx-auto flex w-[992px]">
-				<GithubSection />
-				<TwitterSection />
-			</div>
-			{/* {props.children} */}
+			{props.children}
 			<Footer />
 		</div>
 	);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from "next";
+import { BlogSection } from "src/component/BlogSection";
+import { GithubSection } from "src/component/GithubSection";
+import { PortfolioSection } from "src/component/PortfolioSection";
+import { TitleSection } from "src/component/TitleSection";
+import { TwitterSection } from "src/component/TwitterSection";
+import { Footer } from "src/layout/AppLayout/Footer";
+import { Header } from "src/layout/AppLayout/Header";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+const Home: NextPage = (props) => {
+	return (
+		<div>
+			{/* <Header /> */}
+			<TitleSection />
+			<BlogSection />
+			<PortfolioSection />
+			<div className="mx-auto flex w-[992px]">
+				<GithubSection />
+				<TwitterSection />
+			</div>
+			{/* {props.children} */}
+			{/* <Footer /> */}
+		</div>
+	);
 };
 
 export default Home;
