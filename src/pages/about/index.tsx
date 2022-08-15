38f3ee260badb715066d4af1c78f42c2f28a835e
shn <topdog.shn.01@gmail.com>
2022-08-15 22:33:52 +0900

    ✨ Aboutページ：仮実装

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,18 @@
+// import Link from "next/link";
+import React from "react";
+
+export const About = () => {
+	return (
+		<section className="mb-24 ml-20 mt-10">
+			<h2 className="mb-6 border-b border-gray-200 pb-5 text-left text-2xl font-bold">
+				About
+			</h2>
+			<h3 className="text-bold mb-6 text-xl">Lightsound Shimabu </h3>
+			<p>
+				ITエンジニアYouTuber。神戸大学経営学部卒。未経験から独学でプログラミングを勉強し、新卒でヤフーに入社。2019年に株式会社GameHintを創業。
+			</p>
+		</section>
+	);
+};
+
+export default About;
