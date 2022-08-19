60578363cee8e1408aa1599de31724a91f4fea57
shn <topdog.shn.01@gmail.com>
2022-08-19 20:50:27 +0900

    ✨ BlogSection：仮リンク追加

--- src/component/BlogSection.tsx
+++ src/component/BlogSection.tsx
@@ -9,44 +9,64 @@ export const BlogSection = () => {
 			</h2>
 			<ul>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 			</ul>
 			<div className="text-center">
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,5 @@
 // import Link from "next/link";
+import Link from "next/link";
 import React from "react";
 
 export const Blog = () => {
@@ -9,84 +10,124 @@ export const Blog = () => {
 			</h2>
 			<ul>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 				<li className="mb-6">
-					<h3 className="mb-2 text-[22px]">This is a header</h3>
-					<p className="mb-2">
-						Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-						sint. Velit officia consequat duis enim velit mollit.
-					</p>
-					<p className="text-bold text-gray-400">2022.07.11</p>
+					<Link href={`/blog`}>
+						<a className="hover:text-gray-400">
+							<h3 className="mb-2 text-[22px]">This is a header</h3>
+							<p className="mb-2">
+								Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+								amet sint. Velit officia consequat duis enim velit mollit.
+							</p>
+							<p className="text-bold text-gray-400">2022.07.11</p>
+						</a>
+					</Link>
 				</li>
 			</ul>
 		</section>
