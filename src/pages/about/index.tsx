02f7ead2a692224ae07b27b676a480b61b553987
shn <topdog.shn.01@gmail.com>
2022-08-29 22:48:31 +0900

    💄 レスポンシブ対応

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,7 +3,7 @@ import { Title } from "src/component/Title";
 
 export const About = () => {
 	return (
-		<section className="mb-24 mt-10 w-[992px]">
+		<section className="mx-auto mt-10 mb-16 w-full px-4 lg:mb-24 lg:w-[992px] lg:px-0">
 			<Title>About</Title>
 			<h3 className="text-bold mb-6 text-xl">Lightsound Shimabu </h3>
 			<p>
