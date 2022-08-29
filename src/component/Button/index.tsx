19600a3d64a13fe7ea8bfd372aad2cc8a1ffed96
shn <topdog.shn.01@gmail.com>
2022-08-29 20:56:29 +0900

    🩹 fix

--- src/component/Button/index.tsx
+++ src/component/Button/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Link from "next/link";
 
-export const Button = (props: any) => {
+type ButtonProps = {
+	children: ReactNode;
+};
+
+export const Button = (props: ButtonProps) => {
 	const { children } = props;
+
 	return (
 		<button className="rounded-full bg-black px-5 py-2 text-white">
-			<Link href={"blog"}>
+			<Link href={""}>
 				<a>{children}</a>
 			</Link>
 		</button>
